b4bcb87f9c122938d915ae8522b207ce940a3a8e
Pedro Rendeiro <76977246+pedro-rendeiro@users.noreply.github.com>
2023-08-22 11:04:38 +0000

    page for malu v1

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Hey!!!</h1>
+    return (
+    <>
+        <h1>Oi, Malu! 🌱</h1> 
+        <p>Aqui é o tio Pedro. Deixei esse pedacinho da internet reservado para você.</p>
+        <p>Com o passar do tempo, podemos adicionar por aqui algumas coisas que você goste.</p>
+        <p>Como ainda não conheço seu gosto, fui no básico kkjjkjkkk.</p>
+        <p>Se você gostou da ideia, dá um chute na barriga da mamãe quando possível.</p>
+    </>
+    ) 
 }
 
 export default Home;
